432311d37a106d57f5e5c427addf69b4eb1e9429
larryvsdata <ermanbekaroglu@gmail.com>
2018-08-15 19:28:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ function shout(string){
 function whisper(string){
   return string.toLowerCase()
 }
+
+
+function logShout(string){
+  return string.toUpperCase()
+}
+
+
+function whisper(string){
+  return string.toLowerCase()
+}
